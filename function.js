04b7948417e7edc48c996feeddeaf52a2a9d879c
jorge villalba <j.ivanvillalba@gmail.com>
2021-12-18 21:49:52 -0500

    Se agregaron algunas funcionalidades con js

---
+++ function.js
@@ -0,0 +1,46 @@
+var array = new Array(10);
+var cantidad = 0;
+var incognito = 0;
+
+function iniciar () {
+    incognito = Math.trunc(Math.random()*100);
+    console.log(incognito);
+}
+
+function evaluar () {
+    let num = document.getElementById("entrada").value;
+    cantidad++;
+    console.log(cantidad);
+    array[cantidad] = num;
+
+    if(cantidad<10){
+        if (num<incognito){
+            document.getElementById("ayuda").innerHTML = "El numero digitado esta por debajo del numero a adivinar";
+        }
+        else if (num>incognito){
+            document.getElementById("ayuda").innerHTML = "El numero digitado esta por encima del numero a adivinar";
+        }
+        else if (num=incognito){   
+            document.getElementById("ayuda").innerHTML = "Has adivinado el numero"; 
+            document.getElementById("mensajeFinal").innerHTML = "Has Ganado"; 
+            document.getElementById("numh3").innerHTML = incognito;
+        }
+    }
+    else {
+        document.getElementById("mensajeFinal").innerHTML = "Has perdido el juego";
+        document.getElementById('entrada').disabled = true;
+        document.getElementById('intentar').disabled = true;
+        document.getElementById("numh3").innerHTML = incognito;
+    }
+    
+}
+
+function reiniciar () {
+    iniciar ();
+    document.getElementById('entrada').disabled = false;
+    document.getElementById('intentar').disabled = false;
+    array = new Array(10);
+    cantidad = 0;
+    document.getElementById("numh3").innerHTML = "";
+    document.getElementById("ayuda").innerHTML = "";
+}
